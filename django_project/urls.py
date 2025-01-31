f459731d4ead68ef0ca52880092def5dcb2dbd98
Mahtab Tanim <tanimbsmrstu@gmail.com>
2025-01-31 22:14:14 +0600

    add environment variables

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,4 +15,7 @@ urlpatterns = [
     path(
         "api/v1/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")
     ),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/redoc", SpectacularRedocView.as_view(), name="schema-redoc"),
+    path("api/schema/swagger", SpectacularSwaggerView.as_view(), name="schema-swagger"),
 ]
